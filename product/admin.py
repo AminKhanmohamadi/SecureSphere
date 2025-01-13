e284d2009cfd6109365b07326c887c6cf6fab7ea
amin <aminkhm097@gmail.com>
2025-01-13 18:23:38 +0330

    fix: admin panel for product app

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, ProductAttribute
+from .models import Category, Product, ProductAttribute, ProductRecommendation, ProductImage
 
 
 # Register your models here.
@@ -11,9 +11,20 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProductAttributeInline(admin.TabularInline):
     model = ProductAttribute
-    extra = 0
+    extra = 1
+
+class ProductRecommendationInline(admin.TabularInline):
+    model = ProductRecommendation
+    extra = 1
+    fk_name = 'primary'
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     model = Product
-    inlines = [ProductAttributeInline]
+    list_display = ['title' , 'category' , 'is_public']
+    inlines = [ProductAttributeInline ,ProductImageInline ,ProductRecommendationInline]
