4864f22145a91d9583ccfe319d596893c30f005b
amin <aminkhm097@gmail.com>
2025-01-13 20:32:44 +0330

    partial templates header and main page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
+from unicodedata import category
+
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views import View
+
+from products.models import Category
+
 
 # Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'core/main.html'
+
+
+
+class CategoryPartials(View):
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.all()
+        return render(request , 'core/partials/category_partial.html' , {'categories': categories})
